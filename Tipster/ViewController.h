d6001534a102851a3fe618cc78ba8a3cc9662795
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-06-26 13:20:46 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Lizbeth Alejandra Gonzalez on 6/26/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
